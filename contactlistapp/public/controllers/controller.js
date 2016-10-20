47e2f2d5d5da7266257bbd8cf8a966f268d3e2a2
Danny <dannycornelisse@hotmail.com>
2016-10-20 22:35:28 +0200

    Added functionality to add a new contact to the db and automatically show in view

--- contactlistapp/public/controllers/controller.js
+++ contactlistapp/public/controllers/controller.js
@@ -2,11 +2,28 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 
-    // Get "/contactlist" from server from server.js and when succes exe function
-	$http.get("/contactlist").success(function(response){
-		console.log("I recieved the contactlist data");
-		$scope.contactlist = response;	
-	})
+
+	var refresh = function(){
+		// Get "/contactlist" from server from server.js and when succes exe function
+		$http.get("/contactlist").success(function(response){
+			console.log("I recieved the contactlist data");
+			$scope.contactlist = response;
+			// Clean input boxes after refreshing/clicking add contact
+			$scope.contact = "";
+		});
+	};
+
+	// Init refresh function on page load
+	refresh();
+
+	$scope.addContact = function(){
+		console.log($scope.contact);
+		// Post to contactlist the contact
+		$http.post("/contactlist", $scope.contact).success(function(response){
+			console.log(response);
+			refresh();
+		});
+	};
 
     // Added contactlist property to scope object and set it equal to contactlist var
     //$scope.contactlist = contactlist;
--- contactlistapp/server.js
+++ contactlistapp/server.js
@@ -1,31 +1,45 @@
 // set up new app using express framework
 var express = require('express');
 var app = express();
+// setup mo
+var mongojs = require('mongojs');
+var db = mongojs('contactlist',['contactlist']);
+
+// The body parser is needed to parse js objects to json to put in the db
+var bodyParser = require("body-parser");
 
 //use static content (html, css, js from folder named public)
 app.use(express.static(__dirname + "/public"));
+
+// Use the body parser to parse objects to json
+app.use(bodyParser.json());
+
 // When GET "/contactlist", respond with contactlist
 app.get("/contactlist", function(req, res){
 	console.log('I recieved a get request');
-	var contactlist = [
-		person1 = {
-			name: "Tim", 
-			email: "Tim@email.com",
-			number: "111"
-		},
-		person2 = {
-			name: "emily", 
-			email: "emily@email.com",
-			number: "222"
-		},
-		person3 = {
-			name: "John", 
-			email: "John@email.com",
-			number: "333"
-		}
-	];
-	// Respond
-	res.json(contactlist);
+	// find the data in the contactlist in db
+	db.contactlist.find(function(err, docs){
+		console.log(docs);
+
+
+		// respond with json of docs (docs in contactlist)
+		res.json(docs);
+	});
+});
+
+// app.post("/contactlist", function(req, res){
+// 	// Post new contact to db
+// 	db.contactlist.insert(req.body, function(err, doc){
+// 		// Respond with docs
+// 		res.json(docs);
+// 	});
+// })
+
+app.post('/contactlist', function (req, res) {
+  console.log(req.body);
+  db.contactlist.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
 });
 
 // set up local host on port
