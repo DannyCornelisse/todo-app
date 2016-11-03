43315d10284a6ddf1cecb82fab4dedaf82232af4
Danny <dannycornelisse@hotmail.com>
2016-11-03 22:12:39 +0100

    conpleted contactlist tutorial. will use as seed project for todo app

--- contactlistapp/public/controllers/controller.js
+++ contactlistapp/public/controllers/controller.js
@@ -2,7 +2,7 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 
-
+	// Refresh contactlist
 	var refresh = function(){
 		// Get "/contactlist" from server from server.js and when succes exe function
 		$http.get("/contactlist").success(function(response){
@@ -17,14 +17,36 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 	refresh();
 
 	$scope.addContact = function(){
-		console.log($scope.contact);
-		// Post to contactlist the contact
+		// Post to contactlist the contact and in callback refresh the contact list
 		$http.post("/contactlist", $scope.contact).success(function(response){
-			console.log(response);
 			refresh();
 		});
 	};
 
-    // Added contactlist property to scope object and set it equal to contactlist var
-    //$scope.contactlist = contactlist;
+	// Remove contact based on its id
+	$scope.remove = function(id){
+		// Remove contact based on its id from contactlist table
+		$http.delete("/contactlist/" + id).success(function(response){
+			refresh();
+		});
+	}
+
+	$scope.edit = function(id){
+		console.log(id);
+		$http.get("/contactlist/" + id).success(function(response){
+			// Set the response from the GET req equal to $scope.contact, so it will fill in the inputfields in the view 
+			$scope.contact = response;
+		});
+	}
+	$scope.updateContact = function(){
+		console.log($scope.contact._id);
+		// Get the id of the $scope.contact in the inputfield, then put the $scope.contact  
+		// object (which is in the input fields) in its place in the db
+		$http.put("/contactlist/" + $scope.contact._id, $scope.contact).success(function(response){
+			refresh();
+		});
+	}
+	$scope.deselect = function() {
+		$scope.contact = "";
+	}
 }]);
--- contactlistapp/server.js
+++ contactlistapp/server.js
@@ -1,18 +1,19 @@
 // set up new app using express framework
 var express = require('express');
 var app = express();
-// setup mo
+// setup mongo database with mongojs
 var mongojs = require('mongojs');
+// Create new db with ('name of db','table of db??')
 var db = mongojs('contactlist',['contactlist']);
 
 // The body parser is needed to parse js objects to json to put in the db
 var bodyParser = require("body-parser");
 
-//use static content (html, css, js from folder named public)
-app.use(express.static(__dirname + "/public"));
-
 // Use the body parser to parse objects to json
 app.use(bodyParser.json());
+
+//use static content (html, css, js from folder named public)
+app.use(express.static(__dirname + "/public"));
 
 // When GET "/contactlist", respond with contactlist
 app.get("/contactlist", function(req, res){
@@ -21,25 +22,49 @@ app.get("/contactlist", function(req, res){
 	db.contactlist.find(function(err, docs){
 		console.log(docs);
 
-
 		// respond with json of docs (docs in contactlist)
 		res.json(docs);
 	});
 });
 
-// app.post("/contactlist", function(req, res){
-// 	// Post new contact to db
-// 	db.contactlist.insert(req.body, function(err, doc){
-// 		// Respond with docs
-// 		res.json(docs);
-// 	});
-// })
 
 app.post('/contactlist', function (req, res) {
-  console.log(req.body);
   db.contactlist.insert(req.body, function(err, doc) {
     res.json(doc);
   });
+});
+
+app.delete('/contactlist/:id', function(req, res){
+	// Set id from request as a new variable
+	var id = req.params.id;
+	// Pass id to remove from mongodb
+	db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
+		// Respond with doc, which is the contact item
+		res.json(doc);
+	})
+});
+
+// when GET "contactlist/:id", respond with id
+app.get('/contactlist/:id', function (req, res) {
+  var id = req.params.id;
+  db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.put('/contactlist/:id', function (req, res) {
+	var id = req.params.id;
+  	console.log(req.body.name);
+	db.contactlist.findAndModify({query: {_id: mongojs.ObjectId(id)},
+		update: {$set: {
+			name: req.body.name, 
+			email: req.body.email,
+			number: req.body.number
+		}},
+		new: true}, function(err, doc){
+			res.json(doc);		
+		});
+  
 });
 
 // set up local host on port
