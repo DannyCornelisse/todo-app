8f0d1a60df854ea3ff4b863d8e3d83a804fc51b0
Danny <dannycornelissemsc@gmail.com>
2017-01-24 12:03:31 +0100

    Added login authentication

--- mean-tutorial/public/app/controllers/mainCtrl.js
+++ mean-tutorial/public/app/controllers/mainCtrl.js
@@ -1,5 +1,32 @@
-angular.module('mainController',[])
-
-	.controller('mainCtrl', function(){
+angular.module('mainController',['authServices'])
+	// Take Auth as parameter
+	.controller('mainCtrl', function(Auth, $timeout, $location){
 		
+		var app = this;
+
+		app.doLogin = function(loginData){
+			app.errorMsg = false;
+			app.loading = true;
+			// http post request using $http injection (also injected in function) (post method is provided by userServices.js)
+			Auth.login(app.loginData)
+				// Respond with function to console.log the data from the post
+				.then(function(data){
+					// If get succes back from api
+					if(data.data.success){
+						// Create success message
+						app.loading = false;
+						app.successMsg = data.data.message;
+
+						// Redirect to home with 2sec timeout
+						$timeout(function(){
+							$location.path('/about');
+						}, 2000);
+					} else {
+						// Cretate error Message
+						app.loading = false;
+						app.errorMsg = data.data.message; 
+					}
+				})
+
+		}
 	});
--- mean-tutorial/public/app/services/authServices.js
+++ mean-tutorial/public/app/services/authServices.js
@@ -1,11 +1,11 @@
 angular.module('authServices', [])
-	// Create factory and put $http as a dependency / parameter
+	// Create factory  Auth and put $http as a dependency / parameter
 	.factory('Auth', function($http){
 		var authFactory = {};
 
-		// This service returns a custom post method and takes regData as parameter
-		authFactory.login = function(regData){
-			return $http.post('/api/users', regData)
+		// This service returns a custom post method and takes loginData as parameter
+		authFactory.login = function(loginData){
+			return $http.post('/api/authenticate', loginData)
 		};
 
 		return authFactory;
